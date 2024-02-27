6402fbdcc4bf30a0bce07f442fccc757fe9dad49
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-02-27 17:25:46 +0530

    results of Maths operation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         MathFunction add=()->4+2;
+        MathFunction.printResult(4,2, "add", add);
         MathFunction sub=()->4-2;
+        MathFunction.printResult(4,2,"sub", sub);
         MathFunction mul=() ->4*2;
+        MathFunction.printResult(4,2,"mul",mul);
     }
 }
